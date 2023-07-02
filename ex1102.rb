ba38f350ab1faaa6a60492f1dd59b876c539c49b
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-02 23:49:53 +0900

    11-2-2

---
+++ ex1102.rb
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# 新しいハッシュを作る
+friends = {
+    :shin => "Shin Kuboaki",
+    :shinichirou => "Shinichirou Ooba",
+    :shingo => "Shingo Katori"
+}
+
+# ハッシュの要素を一つずつ処理する
+# 繰り返しの都度、キーをブロック内のローカル変数keyに渡す
+friends.each_key{ |key|
+    # ローカル変数(キー)の値を表示する
+    puts "#{key} "
+}
