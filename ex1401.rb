04fe452c13f214369917490a4c5b479db5c9e19f
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-07-25 23:02:14 +0900

    14-4

--- ex1401.rb
+++ ex1401.rb
@@ -27,25 +27,97 @@ class BookInfo
     end
 end
 
-BookInfoManagerを定義する
+# BookInfoManagerを定義する
 class BookInfoManager
     def initialize
-        @book_info = {}
+        @book_infos = {}
     end
 
     # 蔵書データをセットアップする
     def setUp
 # 複数冊の蔵書データを登録する 
-book_infos = Hash.new
-book_infos["Yamada2005"] = BookInfo.new(
+@book_infos["Yamada2005"] = BookInfo.new(
     "実践アジャイル　ソフトウェア開発法とプロジェクト管理",
     "山田　正樹",
     248,
     Date.new( 2005, 1, 25))
-book_infos["Ooba2006"] = BookInfo.new(
+@book_infos["Ooba2006"] = BookInfo.new(
     "入門LEGO MINDSTORMS NXT レゴブロックで作る動くロボット",
     "大庭 紳一郎",
     164,
     Date.new( 2006, 12, 23))
+    end
 
+    # 蔵書データを登録する
+    def addBookInfo
+        # 蔵書データ1件分のインスタンスを作成する。
+        book_info = BookInfo.new("","",0,Date.new)
+        # 登録するデータを項目ごとに入力する
+        print "\n"
+        print "キー: "
+        key = gets.chomp
+        print "書籍名: "
+        book_info.title = gets.chomp
+        print "著者名: "
+        book_info.author = gets.chomp
+        print "ページ数: "
+        book_info.page = gets.chomp.to_i
+        print "発刊年: "
+        year = gets.chomp.to_i
+        print "発刊月: "
+        month = gets.chomp.to_i
+        print "発刊日: "
+        day = gets.chomp.to_i
+        book_info.publish_date = Date.new( year,month,day )
 
+        # 作成した蔵書データの1件分をハッシュに登録する
+        @book_infos[key] = book_info
+
+    end
+
+    # 蔵書データの一覧を表示する
+    def listAllBookInfos
+        puts "\n----------------"
+        @book_infos.each do |key, info|
+            print info.toFormattedString
+            puts "\n----------------"
+        end
+    end
+
+    # 処理の選択と洗濯後の処理を繰り返す。
+    def run
+        while true
+            # 機能選択画面を表示する
+            print "
+            1. 蔵書データの登録
+            2. 蔵書データの表示
+            9. 終了
+            番号を選んでください(1,2,9):"
+
+            # 文字の入力を待つ
+            num = gets.chomp
+            case
+            when '1' == num
+                # 蔵書データの登録
+                addBookInfo
+                When '2' == num
+                    # 蔵書データの表示
+                    listAllBookInfos
+                    when '9' == num
+                    # アプリケーションの終了
+                    break;
+                else
+                    # 処理選択街画面に戻る
+                end
+            end
+        end
+    end
+
+    #アプリケーションのインスタンスを作る
+    book_info_manager = BookInfoManager.new
+
+    # BookInfoManagerの蔵書データをセットアップする
+    book_info_manager.setUp
+
+    # BookInfoManagerの処理の選択と洗濯後の処理を繰り返す
+    book_info_manager.run
