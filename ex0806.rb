2206fbb67adca5e2f19c0b550709b8ff683840bf
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-10-04 23:54:09 +0900

    120ページまで

---
+++ ex0806.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+require 'date'
+
+# 表示したい蔵書データを作成する(同じ蔵書は各配列の添字が同じ)
+titles = [
+      "実践アジャイル ソフトウェア開発法とプロジェクト管理",
+      "入門LEGO MINDSTORMS NXT レゴブロックで作る動くロボット",
+      ]
+authors = ["山田 正樹","大庭 慎一郎",]
+yomies = ["やまだまさき","おおばしんいちろう",]
+publishers = ["ソフト・リサーチ・センター",
+              "ソフトバンククリエイティブ",
+              ]
+pages = [248,164,]
+prices = [2500,2400,]
+purchase_prices = [2650,2520,]
+
+isbn_10s = ["4883732088","4797338253",]
+isbn_13s = ["978-4883732081","978-4797338256",]
+sizes = ["21 x 14.8 x 2","23 x 18.2 x 1.6",]
+publish_dates = [Date.new(2005,1,25),Date.new(2006,12,23),]
+purchase_dates = [Date.new(2005,2,2),Date.new(2007,1,10),]
+
+# 蔵書データを表示する
+titles.size.times do |i| #titlesの要素文添字をiとしてくりかえす
+  puts "--------------------------"
+  puts "書籍名: " + title
+  puts "著者名: " + author
+  puts "よみがな: " + yomi
+  puts "出版社: " + publisher
+  puts "ページ数: " + pages.to_s + "ページ"
+  puts "販売価格: " + prices.to_s + "円"
+  puts "販売価格: " + prices.to_s + "円"
+  puts "購入価格: " + purchase_prices.to_s + "円"
+  puts "出版年: " + publish_date.year.to_s
+  puts "出版年(長い名前): " + mon_name
+  puts "出版年(短い名前): " + abbr_mon_name
+end
+
