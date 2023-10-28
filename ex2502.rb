3f71504ced2bdcc6a961ebea0456b7e604ff7fa1
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-10-28 20:02:20 +0900

    322ページまで

---
+++ ex2502.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+require 'webrick'
+
+config = (
+  :Port => 8099,
+  :DocumentRoot =>'.',
+)
+
+server = WEBrick::HTTPServer.new(config)
+
+trap(:INT) do
+  server.shutdown
+end
+
+server.start
