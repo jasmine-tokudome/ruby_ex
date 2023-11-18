6a67e3e543c56bd03402de8e6e213522550dc88c
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-11-18 23:17:53 +0900

    208ページ

---
+++ block_args_test.rb
@@ -0,0 +1,24 @@
+def block_args_test
+  yield()
+  yield(1)
+  yield(1,2,3)
+end
+
+puts "ブロック変数を |a|でうけとる"
+block_args_test do |a|
+  p [a]
+end
+
+puts
+
+puts "ブロック変数を |a,b,c|でうけとる"
+block_args_test do |a,b,c|
+  p [a,b,c]
+end
+puts
+
+puts "ブロック変数を|*a|で受け取る"
+block_args_test do |*a|
+  p [a]
+end
+puts
