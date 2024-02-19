52b27cd13bb74f14509c1b360f48aa9747d64ad2
徳留 美帆 <tokudome@pepabo.com>
2024-02-19 23:21:27 +0900

    20240219

---
+++ arry_new.rb
@@ -0,0 +1,4 @@
+array=Array.new(3,'native')
+array[0].reverse!
+
+pp array
--- q39.rb
+++ q39.rb
@@ -1,1 +1,14 @@
-p "abc def 123 ghi 456".scan(/\d+/).length
+a = [1,2,5,7,8]
+b = [1,3,6,7,9]
+c = nil || a & b | a && a | b
+
+p c
+
+d = nil || a & b
+p d
+
+e = nil || a & b | a
+p e
+
+f = nil || a & b | a && a
+p f
