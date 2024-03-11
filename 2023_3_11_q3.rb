d009a7104f4b2258d35358442a070ea5b4dba2a4
徳留 美帆 <tokudome@pepabo.com>
2024-03-11 19:10:59 +0900

    2023-3-11

---
+++ 2023_3_11_q3.rb
@@ -0,0 +1,35 @@
+class C
+  def self._singleton
+    class << C
+      self
+    end
+  end
+end
+
+p C._singleton
+p "=============="
+
+class C
+end
+
+def C._singleton
+  self
+end
+
+p C._singleton
+
+class C
+end
+
+class << C
+  def _singleton
+    self
+  end
+end
+
+p C._singleton
+
+class C
+end
+p C.singleton_class
+
