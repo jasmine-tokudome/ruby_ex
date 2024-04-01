ec1ea15a61fca64a97da02536c13466ac6cf4cde
徳留 美帆 <tokudome@pepabo.com>
2024-04-01 23:53:27 +0900

    2023-4-1

---
+++ 2024_4_1_q13.rb
@@ -0,0 +1,7 @@
+def hoge(*args, &block)
+  block.call(*args)
+end
+
+hoge(1,2,3,4) do |*args|
+  p args.length > 0 ? "hello" : args
+end
---
+++ 2024_4_1_q14.rb
@@ -0,0 +1,12 @@
+class Hoge
+  def initialize
+    @fizz = "bazz"
+  end
+end
+
+puts Hoge.new.inspect
+
+puts Hoge.new
+puts Hoge.new.to_s
+p Hoge.new
+print Hoge.new
