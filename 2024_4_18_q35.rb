b6ad6e89e6d3a25e1c1451208a5c199b588fd0f8
徳留 美帆 <tokudome@pepabo.com>
2024-04-18 22:09:40 +0900

    2023-4-18

---
+++ 2024_4_18_q35.rb
@@ -0,0 +1,28 @@
+module MathConstant
+  PI = 3.14
+end
+
+class Area
+  include MathConstant
+
+  def circle(r)
+    r * r * PI
+  end
+end
+
+area = Area.new
+p area.circle 10
+
+module MathConstant
+  PI = 3.14
+end
+
+class Area
+  def circle(r)
+    r * r * MathConstant::PI
+  end
+end
+
+area = Area.new
+p area.circle 10
+
