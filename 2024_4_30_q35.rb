e600fa2aaf6d49cd0deaac0acb2beed5ac807a55
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-05-01 00:03:16 +0900

    2024_5_1

---
+++ 2024_4_30_q35.rb
@@ -0,0 +1,9 @@
+def greeting
+    "hello"
+ensure
+    puts "Ensure called1"
+
+    "hi"
+end
+
+puts greeting
---
+++ 2024_5_1_q38.rb
@@ -0,0 +1,11 @@
+module Mixin
+    def this_object
+        self
+    end
+end
+
+class Identity
+    include Mixin
+end
+
+p Identity.new.this_object.class
